d803029b633438d7e01bfb2ea9786f5f9ab103f5
christianto88 <christianto88@hotmail.com>
2017-08-17 15:04:09 +0700

    master detail flow

--- app/src/main/java/com/example/sony/bakingapp/recipeDetail.java
+++ app/src/main/java/com/example/sony/bakingapp/recipeDetail.java
@@ -12,11 +12,12 @@ public class recipeDetail extends AppCompatActivity implements recipeFragment.on
     private Recipe recipeData;
     private ArrayList<Ingredients> ingredientsData;
     private ArrayList<Steps> stepsData;
-
+    private boolean mTwoPane;
     public recipeDetail(){}
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_detail);
+
 
         Intent intentThatStartedThisActivity = getIntent();
 
@@ -27,6 +28,21 @@ public class recipeDetail extends AppCompatActivity implements recipeFragment.on
                 stepsData=recipeData.getStepsArrayList();
                 ingredientsData=recipeData.getIngredientsArrayList();
             }
+        }
+        if(findViewById(R.id.tablet_linear)!=null){
+            mTwoPane=true;
+            if(savedInstanceState==null) {
+                ingredientsAndStepsFragment x = new ingredientsAndStepsFragment();
+//                Bundle args = new Bundle();
+//                args.putParcelableArrayList("stepsArray", stepsArrayList);
+//                args.putParcelable("steps", stepsData);
+//                x.setArguments(args);
+                FragmentManager fm = getSupportFragmentManager();
+                fm.beginTransaction().add(R.id.my_container2, x).commit();
+            }
+        }
+        else{
+            mTwoPane=false;
         }
         recipeFragment rf=new recipeFragment();
         Bundle args=new Bundle();
@@ -42,20 +58,38 @@ public class recipeDetail extends AppCompatActivity implements recipeFragment.on
     @Override
     public void onClicked(ArrayList<Ingredients> i,Steps s) {
         if(i!=null) {
-            Class destinationClass = detailActivity.class;
-            Intent intentToStartDetailActivity = new Intent(this.getApplicationContext(), destinationClass);
-            intentToStartDetailActivity.putParcelableArrayListExtra("ingredients", i);
-            intentToStartDetailActivity.putExtra("recipeName", recipeData.getRecipeName());
-            startActivity(intentToStartDetailActivity);
+            if(mTwoPane){
+                ingredientsAndStepsFragment x = new ingredientsAndStepsFragment();
+                Bundle args = new Bundle();
+                args.putParcelableArrayList("ingreData", i);
+                x.setArguments(args);
+                getSupportFragmentManager().beginTransaction().replace(R.id.mycontainer_new,x).commit();
+            }else {
+                Class destinationClass = detailActivity.class;
+                Intent intentToStartDetailActivity = new Intent(this.getApplicationContext(), destinationClass);
+                intentToStartDetailActivity.putParcelableArrayListExtra("ingredients", i);
+                intentToStartDetailActivity.putExtra("recipeName", recipeData.getRecipeName());
+                startActivity(intentToStartDetailActivity);
+            }
         }
         else
         {
-            Class destinationClass = detailActivity.class;
-            Intent intentToStartDetailActivity = new Intent(this.getApplicationContext(), destinationClass);
-            intentToStartDetailActivity.putExtra("steps", s);
-            intentToStartDetailActivity.putParcelableArrayListExtra("stepsData",stepsData);
-            intentToStartDetailActivity.putExtra("recipeName", recipeData.getRecipeName());
-            startActivity(intentToStartDetailActivity);
+            if(mTwoPane){
+                ingredientsAndStepsFragment x = new ingredientsAndStepsFragment();
+                Bundle args = new Bundle();
+                args.putParcelableArrayList("stepsArray",stepsData);
+                args.putParcelable("steps",s);
+                x.setArguments(args);
+                getSupportFragmentManager().beginTransaction().replace(R.id.mycontainer_new,x).commit();
+            }
+            else {
+                Class destinationClass = detailActivity.class;
+                Intent intentToStartDetailActivity = new Intent(this.getApplicationContext(), destinationClass);
+                intentToStartDetailActivity.putExtra("steps", s);
+                intentToStartDetailActivity.putParcelableArrayListExtra("stepsData", stepsData);
+                intentToStartDetailActivity.putExtra("recipeName", recipeData.getRecipeName());
+                startActivity(intentToStartDetailActivity);
+            }
         }
     }
 
